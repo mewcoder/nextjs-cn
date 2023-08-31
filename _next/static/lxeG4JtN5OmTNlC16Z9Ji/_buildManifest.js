4cfd8ee5f25d9813fae93cccdf68c8f88a79f069
mewcoder <mewcoder@users.noreply.github.com>
2023-08-31 11:48:32 +0000

    deploy: 094f24c2b2672b289606ac0b146d2551c8cb0813

---
+++ _next/static/lxeG4JtN5OmTNlC16Z9Ji/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-5d67e88ed3a8dca5.js"],"/_error":["static/chunks/pages/_error-1d28e89ede5693a1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
